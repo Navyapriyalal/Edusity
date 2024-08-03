870c71343056c2a1a8b4f299f9613141b099dbf3
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-08-04 00:15:49 +0530

    'commited'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar.jsx'
 import Hero from './components/Hero/Hero.jsx'
+import Program from './components/Programs/Program.jsx'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <div className="container">
+        <Program />
+      </div>
     </div>
   )
 }
