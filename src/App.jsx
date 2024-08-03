f46f3cdc29e6d3f69c8066b00ac26a6f19a94fc1
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-08-04 00:35:31 +0530

    'commited'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Program from './components/Programs/Program.jsx'
+import Title from './components/Title/Title.jsx'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <div className="container">
+        <Title subTitle={'OUR PROGRAM'} title={'What We Offer'}/>
         <Program />
       </div>
     </div>
