ffd0030825b1381596f74c102d4a39c170cff7d3
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-08-05 22:08:06 +0530

    'commited'

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,10 +13,16 @@ const NavBar = () => {
     })
   },[])
 
+  const [mobilemenu , setMobilemenu] = useState(false)
+
+  const togglemenu = () => {
+    mobilemenu ? setMobilemenu(false) : setMobilemenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt='' className='logo'/>
-      <ul>
+      <ul className={mobilemenu ? '' : 'hide-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
         <li><Link to='programs' smooth={true} offset={-250} duration={500} >Program</Link></li>
         <li><Link to='about' smooth={true} offset={-130} duration={500} >About us</Link></li>
@@ -24,7 +30,7 @@ const NavBar = () => {
         <li><Link to='testimonials' smooth={true} offset={-240} duration={500} >Testimonials</Link></li>
         <li><Link to='contact' smooth={true} offset={-230} duration={500} ><button className='btn'>Contact us</button></Link></li>
       </ul>
-      <img src={menu_icon} alt="" className='menu-icon'/>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={togglemenu}/>
     </nav>
   )
 }
