60b794f739328ba3bbdc87a4c9b228e8f5521e95
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-08-06 00:12:32 +0530

    'commited'

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Video.css'
 import play_video from '../../assets/video.mp4'
 
 const Video = ({ playState,setPlayState }) => {
+  const player = useRef(null);
+
+  const closePlayer = (e) => {
+    if (e.target === player.current){
+        setPlayState(false);
+    }
+  }
+
   return (
-    <div className={`video-play ${playState ? '' : 'hide'}`}>
+    <div className={`video-play ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
       <video src={play_video} autoPlay muted controls></video>
     </div>
   )
